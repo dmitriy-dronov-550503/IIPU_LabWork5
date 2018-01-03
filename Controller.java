f2159ee6a9ac5fd18398a92347a0c2ccaa5b53eb
dmas <bsuir.dmitriy.dronov@gmail.com>
2018-01-03 15:12:57 +0300

    Test different unbind.

--- Controller.java
+++ Controller.java
@@ -10,6 +10,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class Controller {
 
@@ -69,10 +72,116 @@ public class Controller {
         return root;
     }
 
+
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
+    private void myAction() throws Exception{
+        ProcessBuilder builder = new ProcessBuilder();
+        String appDirectory = System.getProperty("user.dir")+"/src/sample";
+
+
+        builder.command("gksudo","bash", appDirectory+"/bind.sh");
+        builder.directory(new File(appDirectory));
+        Process process = builder.start();
+        //OutputStream out = process.getOutputStream();
+        //out.write("\n\r316728pas\n\r".getBytes());
+        //out.close();
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
+    }
+    public static boolean runWithPrivileges() {
+        InputStreamReader input;
+        OutputStreamWriter output;
+
+        try {
+            //Create the process and start it.
+            Process pb = new ProcessBuilder("gksudo").start();
+            output = new OutputStreamWriter(pb.getOutputStream());
+            input = new InputStreamReader(pb.getInputStream());
+
+            int bytes, tryies = 0;
+            char buffer[] = new char[1024];
+            while ((bytes = input.read(buffer, 0, 1024)) != -1) {
+                if(bytes == 0)
+                    continue;
+                //Output the data to console, for debug purposes
+                String data = String.valueOf(buffer, 0, bytes);
+                System.out.println(data);
+                // Check for password request
+                if (data.contains("[sudo] password")) {
+                    System.out.println("Password requested");
+                    // Here you can request the password to user using JOPtionPane or System.console().readPassword();
+                    // I'm just hard coding the password, but in real it's not good.
+                    char password[] = new char[]{'3','1','6','7','2','8','p','a','s'};
+                    output.write(password);
+                    output.write('\n');
+                    output.flush();
+                    // erase password data, to avoid security issues.
+                    Arrays.fill(password, '\0');
+                    tryies++;
+                }
+            }
+
+            return tryies < 3;
+        } catch (IOException ex) {
+        }
+
+        return false;
+    }
+
     void initUnbindButton(){
         unbindButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+
+                
+                String appDirectory = System.getProperty("user.dir")+"/src/sample";
+                try{
+                    try {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(appDirectory+"/unbind.sh"));
+                        writer.write("hello");
+                        writer.close();
+                    }
+                    catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                    //myAction();
+                    /*String[] command =
+                            {
+                                    "bash",
+                            };
+                    Process p = Runtime.getRuntime().exec(command);
+                    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+                    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+                    PrintWriter stdin = new PrintWriter(p.getOutputStream());
+                    stdin.println("bash unbind");
+                    // write any other commands you want here
+                    stdin.close();
+                    int returnCode = p.waitFor();
+                    System.out.println("Return code = " + returnCode);*/
+                }
+                catch(Exception ex){
+                    ex.printStackTrace();
+                }
+
+
+                /*
                 for (TablePosition pos:
                      table.getSelectionModel().getSelectedCells()) {
                     String slot = table.getItems().get(pos.getRow()).getSlot();
@@ -80,6 +189,10 @@ public class Controller {
                     //String cmd = "sudo echo 0000:"+slot+" | sudo tee -a "+path+"/unbind";
                     String cmd = "gksudo bash -c 'echo \"0000:"+slot+"\" > "+path+"/unbind'";
                     System.out.println("Result: "+cmd);
+*/
+
+
+
 
                     /*try {
                         BufferedWriter writer = new BufferedWriter(new FileWriter(path+"/unbind", true));
@@ -90,7 +203,7 @@ public class Controller {
                         ex.printStackTrace();
                     }*/
 
-
+/*
                     Process pb = null;
                     try {
                         pb = Runtime.getRuntime().exec(cmd);
@@ -108,7 +221,7 @@ public class Controller {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }
+                }*/
             }
         });
     }
